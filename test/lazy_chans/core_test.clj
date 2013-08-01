46b66fb7f7f11e4c95590558bedbb94ab98baa72
Darrell Hamilton <darrell.noice@gmail.com>
2013-08-01 14:33:07 -0700

    basic test coverage

--- test/lazy_chans/core_test.clj
+++ test/lazy_chans/core_test.clj
@@ -1,7 +1,10 @@
 (ns lazy-chans.core-test
   (:require [clojure.test :refer :all]
-            [lazy-chans.core :refer :all]))
+            [lazy-chans.core :refer :all]
+            [clojure.core.async :refer (go)]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest lazy-seq-of-channel-messages
+  (testing "lazy-chans!! returns a lazy-seq of channel results"
+           (let [s (lazy-chans!! (go 1) (go 2) (go 3))]
+             (is (= '(1 2 3) (sort s)))
+             (is (= 3 (count s))))))
