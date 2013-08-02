c62eb8aa4361f0b93174d818ecefc13f850aaa65
Darrell Hamilton <darrell.noice@gmail.com>
2013-08-02 14:33:24 -0700

    incremental reducing of channel messages

--- src/lazy_chans/core.clj
+++ src/lazy_chans/core.clj
@@ -1,5 +1,5 @@
 (ns lazy-chans.core
-  (:require [clojure.core.async :refer (alts! alts!!)]))
+  (:require [clojure.core.async :refer (go <! alts! alts!!)]))
 
 (defn lazy-chans!! [cs]
   (lazy-seq
@@ -8,3 +8,32 @@
       (if (empty? remaining)
         [value]
         (cons value (lazy-chans!! remaining))))))
+
+(defn- reduce-additional-messages [a f ch]
+  (go
+    (loop []
+      (if-let [msg (<! ch)]
+        (do
+          (swap! a f msg)
+          (recur))))))
+
+(defn reduce-chan
+  ([f v ch] (let [result (atom v)]
+    (go
+      (if-let [initial-message (<! ch)]
+        (do
+          (swap! result f initial-message)
+          (reduce-additional-messages result f ch))
+        (reset! result (f v))))
+    result))
+
+  ([f ch] (let [result (atom nil)]
+    (go
+      (if-let [initial-message (<! ch)]
+        (if-let [second-message (<! ch)]
+          (do
+            (reset! result (f initial-message second-message))
+            (reduce-additional-messages result f ch))
+          (reset! result initial-message))
+        (reset! result (f))))
+    result)))
--- test/lazy_chans/core_test.clj
+++ test/lazy_chans/core_test.clj
@@ -1,10 +1,75 @@
 (ns lazy-chans.core-test
   (:require [clojure.test :refer :all]
             [lazy-chans.core :refer :all]
-            [clojure.core.async :refer (go)]))
+            [clojure.core.async :refer (close! go chan >!!)]))
 
 (deftest lazy-seq-of-channel-messages
   (testing "lazy-chans!! returns a lazy-seq of channel results"
            (let [s (lazy-chans!! [(go 1) (go 2) (go 3)])]
              (is (= '(1 2 3) (sort s)))
              (is (= 3 (count s))))))
+
+(deftest incremental-reudce-chan
+  (testing "can reduce with no messages"
+           (let [f (fn [] true)
+                 ch (chan)
+                 result (reduce-chan f ch)]
+             (close! ch)
+             (Thread/sleep 100)
+             (is (= true @result))))
+
+  (testing "can reduce with one messages"
+           (let [f (fn [a] a)
+                 ch (chan)
+                 result (reduce-chan f ch)]
+             (>!! ch true)
+             (close! ch)
+             (Thread/sleep 100)
+             (is (= true @result))))
+
+  (testing "can reduce two messages"
+           (let [f +
+                 ch (chan)
+                 result (reduce-chan f ch)]
+             (>!! ch 1)
+             (>!! ch 2)
+             (Thread/sleep 100)
+             (is (= 3 @result))))
+
+   (testing "can reduce more messages"
+           (let [f +
+                 ch (chan)
+                 result (reduce-chan f ch)]
+             (>!! ch 1)
+             (>!! ch 2)
+             (>!! ch 3)
+             (close! ch)
+             (Thread/sleep 100)
+             (is (= 6 @result))))
+
+   (testing "can reduce with initial value and no messages"
+           (let [f (fn [a] a)
+                 ch (chan)
+                 result (reduce-chan f 1 ch)]
+             (close! ch)
+             (Thread/sleep 100)
+             (is (= 1 @result))))
+
+   (testing "can reduce with initial value and one message"
+           (let [f +
+                 ch (chan)
+                 result (reduce-chan f 1 ch)]
+             (>!! ch 1)
+             (close! ch)
+             (Thread/sleep 100)
+             (is (= 2 @result))))
+
+   (testing "can reduce with initial value and more message"
+           (let [f +
+                 ch (chan)
+                 result (reduce-chan f 1 ch)]
+             (>!! ch 1)
+             (>!! ch 1)
+             (close! ch)
+             (Thread/sleep 100)
+             (is (= 3 @result)))))
